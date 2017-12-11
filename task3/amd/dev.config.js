deff8b17eaf7956e23eeb9cc8ddd32a10d9ca3ce
Andrei Pastukhou <31850884+andrei-pastukhou@users.noreply.github.com>
2017-12-11 11:50:10 +0300

    Use sintax version > 2 in webpack's configs. Define NODE_ENV variable.

--- task3/amd/dev.config.js
+++ task3/amd/dev.config.js
@@ -1,47 +1,9 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-module.exports = {
-  entry: {
-    index: './src/index',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.(sass|scss)$/,
-        //loader:'style-loader!css-loader!sass-loader',
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader!sass-loader'
-        }),
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template : './src/index.template.ejs',
-      inject: 'body',
-      title: 'APInews reading...'
-    }),
-    new ExtractTextPlugin({
-      filename:  (getPath) => {
-        return getPath('css/[name].css');
-      },
-      allChunks: false,
-    }),
+const merge = require('webpack-merge');
+const baseConfig = require('./base.config.js');
 
-  ],
-  output: {
-    filename: 'js/[name].bundle.js',
-    chunkFilename: 'js/[name].chunk.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+module.exports = merge(baseConfig, {
+
   devtool: 'eval-source-map',
 
   devServer: {
@@ -49,4 +11,4 @@ module.exports = {
     contentBase: path.join(__dirname, "dist"),
     port: '3001',
   }
-};
+});
--- task3/amd/src/index.js
+++ task3/amd/src/index.js
@@ -1,15 +1,13 @@
 import './scss/bootstrap-start-page.scss';
 
-function component() {
-  const element = document.createElement('div');
+function component(document) {
   const button = document.getElementById('buttonStartViewNews');
-  button.onclick = (e) => {
+  button.addEventListener('click',(e) => {
     require.ensure(['./newsAPI'], (require) => {
       let newsAPI = require('./newsAPI').default;
       newsAPI();
-   });
-  };
-  return element;
+    });
+  });
 }
 
-document.getElementById('news').appendChild(component());
+component(document);
