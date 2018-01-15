ad9d43256faf7c0a44bda25ab5e0e924534c695e
Andrei Pastukhou <31850884+andrei-pastukhou@users.noreply.github.com>
2018-01-15 09:42:56 +0300

    task5 ready

--- task4/redux/src/constants/index.js
+++
@@ -1,5 +0,0 @@
-//define constants for action.type.
-export const FETCH_NEWS = 'FETCH_NEWS';
-export const FETCH_NEWS_SUCCESS = 'FETCH_NEWS_SUCCESS';
-export const FETCH_NEWS_ERROR = 'FETCH_NEWS_ERROR';
-export const CLICK_LINK = 'CLICK_LINK';
--- task4/redux/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import channels from './channels';
-import news from './news';
-
-const rootReducer = {
-  'channels' : channels,
-  'news' : news
-};
-
-export default rootReducer;
