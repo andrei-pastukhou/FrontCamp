6165bedb4fe4f2d6d1955f5e627b94fa5896203d
Andrey PAstukhou <andrey-brest@yandex.ru>
2018-03-18 09:47:54 +0300

    add date and status to list of task. create second listo to view

--- task8.1/todo/todo1.js
+++ task8.1/todo/todo1.js
@@ -1,16 +1,41 @@
 var app = angular.module('toDoApp', [])
 
 app.factory("todoFactory", function(){
-    var taskList = ["New Delhi", "Mumbai", "Kolkata", "Chennai"];
+    var taskList = [
+        {
+            text: 'task1',
+            statusDone: false,
+            date: new Date("2018-03-17T12:00:00.000Z")
+        },
+        {
+            text: 'task2',
+            statusDone: false,
+            date: new Date("2018-03-16T12:00:00.000Z")
+        },
+        {
+            text: 'task3',
+            statusDone: false,
+            date: new Date("2018-03-15T12:00:00.000Z")
+        }
+
+    ]
     return {
         getTasks: function getTasks() {
              return taskList;
         },
         addTask: function addTask(text){
-            taskList.push(text);
+            taskList.push({
+                text: text,
+                statusDone: false,
+                date: new Date()
+            });
         },
-        removeTask: function removeTask(text){
-            taskList.splice(taskList.indexOf(text), 1)
+        doneTask: function doneTask(task){
+            task.statusDone = !task.statusDone;
+        },
+        removeTask: function removeTask(task){
+            console.log(task);
+            taskList.splice(taskList.indexOf(task), 1)
     	}
     };
 });
@@ -26,8 +51,13 @@ app.controller('toDoController', ['$scope', 'todoFactory', function ($scope, tod
         $scope.newTaskName = '';
     };
 
-    $scope.removeTask = function (text) {
-        todoFactory.removeTask(text);
+    $scope.removeTask = function (task) {
+        todoFactory.removeTask(task);
+    };
+
+    $scope.doneTask = function (task) {
+        todoFactory.doneTask(task);
     }
+
 }]);
 
